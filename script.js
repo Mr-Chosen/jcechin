6797661e002c13bb42ce7f73051eb96ca75d0c7d
Mr-Chosen <jcarlechin@gmail.com>
2024-09-19 10:10:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ function openMyLinkProfile(url) {
    window.open(url, '_blank');
    return false;
 }
+
+const topButton = document.getElementById('topButton');
+const topOfPage = document.getElementById('topOfPage');
+
+function handleLikeButtonClick() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+topButton.addEventListener('click', handleLikeButtonClick);
+function toggleLikeButtonVisibility() {
+  if (window.scrollY > 100) {
+    topButton.style.display = 'block';
+  } else {
+    topButton.style.display = 'none';
+  }
+}
+toggleLikeButtonVisibility();
+window.addEventListener('scroll', toggleLikeButtonVisibility);
